63b722174dded7970cb1930006ba6a7e4a3ff9b1
Juan Aramburo <2020030187@upsin.edu.mx>
2024-11-20 20:58:34 -0700

    Modificacion de pagina productos e implementacion de JS

--- js/main.js
+++ js/main.js
@@ -1,79 +1,17 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  import { getDatabase ,onValue, ref as refS, set, child, get, update, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyCATnEerBt8P-jw6TUvOLZOSEzih8oGOGk",
-    authDomain: "proyectofinal-e8310.firebaseapp.com",
-    databaseURL: "https://proyectofinal-e8310-default-rtdb.firebaseio.com",
-    projectId: "proyectofinal-e8310",
-    storageBucket: "proyectofinal-e8310.firebasestorage.app",
-    messagingSenderId: "934654887767",
-    appId: "1:934654887767:web:d87e972fc868bc6a4d2012"
+    apiKey: "AIzaSyCGiusEPpA55h_pXOoA7qgJBaV9KAn0ynU",
+    authDomain: "proyectoweb-4a027.firebaseapp.com",
+    projectId: "proyectoweb-4a027",
+    storageBucket: "proyectoweb-4a027.firebasestorage.app",
+    messagingSenderId: "151310724193",
+    appId: "1:151310724193:web:718ba3e29cb138b4e40e20"
   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const db = getDatabase(app);
-
-  var numSerie = 0;
-  var marca = "";
-  var modelo = "";
-  var descripcion = "";
-  var urlImag = "";
-
-  //funciones
-  function leerInputs(){
-    numSerie = document.getElementById("txtNumSerie").value;
-    marca = document.getElementById("txtMarca").value;
-    modelo = document.getElementById("txtModelo").value;
-    descripcion = document.getElementById("txtDescripcion").value;
-    urlImag = document.getElementById("txtURL").value;
-  }
-
-  function mostrarMensaje(mensaje){
-    var mensajeElement = document.getElementById("mensaje");
-    mensajeElement.textContent = mensaje;
-    mensajeElement.style.display = "block";
-    setTimeout(()=>{
-      mensajeElement.style.display = "none"
-    },1000);
-  }
-
-  //agregar producto a la base de datos
-  const btnAgregar = document.getElementById("btnAgregar");
-  btnAgregar.addEventListener("click", insertarProducto);
-
-  function insertarProducto(){
-    alert("ingrese a add db");
-    leerInputs();
-    //validar
-    if(numSerie ==="" || marca ==="" || modelo ==="" || descripcion ===""){
-      mostrarMensaje("faltaron datos por capturar");
-      return;
-    }
-    // --- funcion de Firebase para agregar registro
-    set{
-      refS(db,"Automoviles/" + numSerie),
-      {
-        /*
-        Realizar json con los campos y datos de la tabla
-        campo: valor 
-        */
-
-        numSerie:numSerie,
-        marca:marca,
-        modelo:modelo,
-        descripcion:descripcion,
-        urlImag:urlImag
-      }
-    }.then(()=>{
-      alert("Se agrego con exito");
-    }).catch ((error)=>{
-      alert("Ocurrio un error");
-    });
-  }
---
+++ js/productos.js
@@ -0,0 +1,112 @@
+var menu = [
+    {"tamaños": "Medium Regular/Thin Crust", "uningrediente": "$179", "dosingredientes": "$219", "cincoingredientes": "$259"},
+    {"tamaños": "Medium Stuffed Crust", "uningrediente": "$209", "dosingredientes": "$249", "cincoingredientes": "$289"},
+    {"tamaños": "Large Regular/Thin Crust", "uningrediente": "$189", "dosingredientes": "$239", "cincoingredientes": "$289"},
+    {"tamaños": "Large Stuffed Crust", "uningrediente": "$229", "dosingredientes": "$279", "cincoingredientes": "$329"},
+    {"tamaños": "Large Deep Dish", "uningrediente": "$199", "dosingredientes": "$249", "cincoingredientes": "$299"},
+    {"tamaños": "Extra Large Regular/Thin Crust", "uningrediente": "$264", "dosingredientes": "$314", "cincoingredientes": "$364"},
+    {"tamaños": "Extra Large Stuffed Crust", "uningrediente": "$342", "dosingredientes": "$392", "cincoingredientes": "$442"},
+]
+  
+var combinaciones = [
+    {"especialidades": "American", "Ingredientes": "Pepperoni, Mushroom, Extra Cheese"},
+    {"especialidades": "Meat Lovers", "Ingredientes": "Pepperoni, Ham, Salami"},
+    {"especialidades": "Vegetariana", "Ingredientes": "Pimiento, Cebolla, Champiñones, Aceitunas negras"},
+    {"especialidades": "Mexicana", "Ingredientes": "Cebolla, Chorizo, Carne Molida, Jalapeño"},
+    {"especialidades": "Hawaiiana", "Ingredientes": "Jamon, Piña, Extra Queso"},
+    {"especialidades": "Brother's burger", "Ingredientes": "Tocino, Carne Molida, Extra Queso"},
+    {"especialidades": "Maui", "Ingredientes": "Jamon, Piña, Jalapeño, Tocino"},
+    {"especialidades": "4 Quesos", "Ingredientes": "Queso Mozarella, Queso Crema, Queso Parmesano, Queso Cheddar"},
+    {"especialidades": "Brother's Especial", "Ingredientes": "Pepperoni, Jamon, Pimiento, Cebolla, Champiñones, Aceitunas, Extra Queso, Aceitunas negras, Carne Molida"},
+    {"especialidades": "Deluxe", "Ingredientes": "Pepperoni, Pimiento, Cebolla, Champiñones, Carne Molida"},
+]
+  
+var adicionales = [
+    {"adicional": "Alitas BBQ", "precio": "$109"},
+    {"adicional": "Alitas Buffalo", "precio": "$109"},
+    {"adicional": "Papotas", "precio": "$59"},
+    {"adicional": "Chessy Bread", "precio": "$79"},
+    {"adicional": "Canelazos", "precio": "$59"},
+    {"adicional": "Bread Sticks", "precio": "$59"},
+]
+
+var bebidas = [
+    {"bebida": "Coca-Cola 600ml", "precio" : "$30"},
+    {"bebida": "Coca-Cola Light 600ml", "precio" : "$30"},
+    {"bebida": "Coca-Cola Zero 600ml", "precio" : "$30"},
+    {"bebida": "Coca-Cola 2L", "precio" : "$55"},
+    {"bebida": "Sprite 600ml", "precio" : "$30"},
+    {"bebida": "Sprite 2L", "precio" : "$55"},
+]
+
+const btnPizzas = document.getElementById("btnPizzas");
+const btnAdicional = document.getElementById("btnAdicionales");
+const btnBebidas = document.getElementById("btnBebidas");
+
+function limpiarTablas() {
+    document.getElementById("tablaTamaños").querySelector("tbody").innerHTML = "";
+    document.getElementById("tablaCombinaciones").querySelector("tbody").innerHTML = "";
+    document.getElementById("tablaAdicionales").querySelector("tbody").innerHTML = "";
+    document.getElementById("tablaBebidas").querySelector("tbody").innerHTML = "";
+}
+
+function tablaTamaño(){
+    limpiarTablas();
+    let tabla = document.getElementById("tablaTamaños").querySelector("tbody");
+    tabla.innerHTML = 
+    `<tr>
+      <th>Tamaño</th>
+      <th>1 Ingrediente</th>
+      <th>2-4 Ingredientes</th>
+      <th>5-9 Ingredientes</th>
+    </tr>`;
+    for(let con = 0; con < menu.length; con++){
+       tabla.innerHTML += "<td>" + menu[con].tamaños + "<td>" + menu[con].uningrediente + "<td>" + menu[con].dosingredientes + "<td>" + menu[con].cincoingredientes;
+    }
+}
+
+function tablaCombinaciones(){
+    limpiarTablas();
+    tablaTamaño();
+    let tablaco = document.getElementById("tablaCombinaciones").querySelector("tbody");
+    tablaco.innerHTML = 
+    `<tr>
+      <th>Especialidades</th>
+      <th>Ingredientes</th>
+    </tr>`;
+    for(let con = 0; con < combinaciones.length; con++){
+       tablaco.innerHTML += "<td>" + combinaciones[con].especialidades + "<td>" + combinaciones[con].Ingredientes;
+    }
+}
+
+function tablaAdicional(){
+    limpiarTablas();
+    let tablaadi = document.getElementById("tablaAdicionales").querySelector("tbody");
+    tablaadi.innerHTML = 
+    `<tr>
+      <th>Extras</th>
+      <th>Precio</th>
+    </tr>`;
+    for(let con = 0; con < adicionales.length; con++){
+       tablaadi.innerHTML += "<td>" + adicionales[con].adicional + "<td>" + adicionales[con].precio;
+    }
+}
+
+function tablaBebidas(){
+    limpiarTablas();
+    let tablabebida = document.getElementById("tablaBebidas").querySelector("tbody");
+    tablabebida.innerHTML = 
+    `<tr>
+      <th>Bebidas</th>
+      <th>Precio</th>
+    </tr>`;
+    for(let con = 0; con < bebidas.length; con++){
+       tablabebida.innerHTML += "<td>" + bebidas[con].bebida + "<td>" + bebidas[con].precio;
+    }
+}
+
+
+btnPizzas.addEventListener("click", tablaTamaño);
+btnPizzas.addEventListener("click", tablaCombinaciones);
+btnAdicional.addEventListener("click", tablaAdicional);
+btnBebidas.addEventListener("click", tablaBebidas);

e770568ef7646b9986319c64a44ac872eebc8c31
Juan Aramburo <2020030187@upsin.edu.mx>
2024-11-21 17:55:36 -0700

    Implementacion de base de datos

---
+++ js/especialidad.js
@@ -0,0 +1,243 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getDatabase, onValue, ref as refS, set, child, get, update, remove } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-storage.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB3RXfHp1g81mT8kLq40OfhlEneaDGemgE",
+    authDomain: "proyecto-ede3c.firebaseapp.com",
+    databaseURL: "https://proyecto-ede3c-default-rtdb.firebaseio.com",
+    projectId: "proyecto-ede3c",
+    storageBucket: "proyecto-ede3c.firebasestorage.app",
+    messagingSenderId: "1038275061124",
+    appId: "1:1038275061124:web:dc95692d082f199dfc69d1"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage();
+
+var idEspecialidad = 0;
+var especialidad = "";
+var ingredientes = "";
+var urlimg = "";
+
+const btnAgregar = document.getElementById("btnAgg");
+const btnBuscar = document.getElementById("btnBus");
+const btnActualizar = document.getElementById("btnAct");
+const btnMostrar = document.getElementById("btnMostrar");
+const btnBorrar = document.getElementById("btnBorr");
+const imagenInput = document.getElementById("imagen");
+const btnImagen = document.getElementById("btnSubir");
+const ProgressDiv = document.getElementById("progress");
+
+function limpiarInput(){
+  idEspecialidad = document.getElementById("txtId").value = "";
+  especialidad = document.getElementById("txtEspecialidad").value = "";
+  ingredientes = document.getElementById("txtIngredientes").value = "";
+  urlimg = document.getElementById("txturl").value = "";
+}
+
+function leerInput(){
+    idEspecialidad = document.getElementById("txtId").value;
+    especialidad = document.getElementById("txtEspecialidad").value;
+    ingredientes = document.getElementById("txtIngredientes").value;
+    urlimg = document.getElementById("txturl").value;
+}
+
+function mostrarMensaje(mensaje){
+  var mensajeElement = document.getElementById("mensaje");
+  mensajeElement.textContent = mensaje;
+  mensajeElement.style.display = "block";
+  setTimeout(()=>{
+      mensajeElement.style.display = "none"},1000);
+}
+
+function insertarProducto() {
+  leerInput();
+  // Validar
+  if (idEspecialidad === "" || especialidad === "" || ingredientes === "") {
+      mostrarMensaje("Faltaron datos por capturar");
+      return;
+  }
+
+  //--- Función de Firebase para agregar registro
+  set(
+      refS(db, 'Especialidades/' + idEspecialidad),
+      {
+          // Datos a guardar
+          idEspecialidad:idEspecialidad,
+          especialidad:especialidad,
+          ingredientes:ingredientes,
+          urlimg:urlimg,
+      }
+  ).then(() => {
+      alert("Se agregó con éxito");
+      limpiarInput(); // Limpiar los campos después de agregar
+      ListarEspecialidades(); // Llamar a la función para actualizar la tabla
+  }).catch((error) => {
+      alert("Ocurrió un error");
+  });
+}
+
+function escribirEspecialidad(){
+  document.getElementById("txtEspecialidad").value = especialidad;
+  document.getElementById("txtIngredientes").value = ingredientes;
+  document.getElementById("txturl").value = urlimg;
+}
+
+function BuscarEspecialidad(){
+    let idEspecialidad = document.getElementById("txtId").value.trim();
+    if(idEspecialidad === ""){
+        mostrarMensaje("No se ingreso una ID");
+        return;
+    }
+    const dbref = refS(db);
+    get(child(dbref, 'Especialidades/' + idEspecialidad)).then((snapshot) =>{
+        if(snapshot.exists()){
+            especialidad = snapshot.val().especialidad;
+            ingredientes = snapshot.val().ingredientes;
+            urlimg = snapshot.val().urlimg;
+            escribirEspecialidad();
+        } else {
+            limpiarInput();
+            mostrarMensaje("El producto con codigo " + idEspecialidad + "no existe.")
+        }
+    });
+}
+
+function ListarEspecialidades() {
+    const dbref = refS(db, 'Especialidades');
+    const tabla = document.getElementById("tablaEspecialidades");
+    const tbody = tabla.querySelector("tbody");
+    tbody.innerHTML =
+    `<tr>
+    <th>ID</th>
+    <th>Especialidad</th>
+    <th>Ingredientes</th>
+    <th></th>
+    </tr>`;
+
+    onValue(dbref, (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            const childKey = childSnapshot.key;
+            const data = childSnapshot.val();
+
+            var fila = document.createElement("tr");
+
+            // Celda para el número de serie
+            var celdaID = document.createElement("td");
+            celdaID.textContent = data.idEspecialidad;
+            fila.appendChild(celdaID);
+
+            // Celda para la marca
+            var celdaEspecialidad = document.createElement("td");
+            celdaEspecialidad.textContent = data.especialidad;
+            fila.appendChild(celdaEspecialidad);
+
+            // Celda para el modelo
+            var celdaIngredientes = document.createElement("td");
+            celdaIngredientes.textContent = data.ingredientes;
+            fila.appendChild(celdaIngredientes);
+
+            // Celda para la imagen
+            var celdaImagen = document.createElement("td");
+            var imagen = document.createElement("img");
+            imagen.src = data.urlimg;
+            imagen.width = 100;
+            celdaImagen.appendChild(imagen);
+            fila.appendChild(celdaImagen);
+
+            // Agregar la fila al cuerpo de la tabla
+            tbody.appendChild(fila);
+        });
+    }, { onlyOnce: true });
+}
+
+function actualizarEspecialidad(){
+    leerInput();
+    if(idEspecialidad ==="" || especialidad ==="" || ingredientes ===""){
+        mostrarMensaje("Favor de capturar toda la informacion.");
+        return;
+    }
+    update(refS(db, 'Especialidades/' + idEspecialidad),{
+        idEspecialidad:idEspecialidad,
+        especialidad:especialidad,
+        ingredientes:ingredientes,
+        urlimg:urlimg
+    }).then(()=>{
+        mostrarMensaje("Se actualizo con exito.");
+        limpiarInput();
+        ListarEspecialidades();
+    }).catch((error) =>{
+        mostrarMensaje("Ocurrio un error: " + error)
+    });
+}
+
+function mostrarTablaEspecialidad(){
+    ListarEspecialidades();
+}
+
+function eliminarEspecialidad(){
+    let idEspecialidad = document.getElementById("txtId").value.trim();
+    if(idEspecialidad === ""){
+        mostrarMensaje("No se ingreso un codigo valido.");
+        return;
+    }
+
+    const dbref = refS(db);
+    get(child(dbref, 'Especialidades/' + idEspecialidad)).then((snapshot) =>{
+        if(snapshot.exists()){
+            remove(refS(db, 'Especialidades/' + idEspecialidad))
+            .then(() => {
+                mostrarMensaje("Producto eliminado con exito.");
+                limpiarInput();
+                ListarEspecialidades();
+            })
+            .catch((error) => {
+                mostrarMensaje("Ocurrio un error al eliminar el producto: " + error);
+            });
+        } else {
+            limpiarInput();
+            mostrarMensaje("El producto con ID " + idEspecialidad + "no existe.");
+        }
+    });
+    ListarEspecialidades();
+}
+
+btnImagen.addEventListener("click", (event) =>{
+    event.preventDefault();
+    const file = imagenInput.files[0];
+
+    if(file) {
+        const storageRef = ref(storage, file.name);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed',(snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            ProgressDiv.textContent = "Progreso: " + progress.toFixed(2) + "%";
+        },(error) => {
+            console.error(error);
+        }, () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                txturl.value = downloadURL;
+                setTimeout(() => {
+                    ProgressDiv.textContent = "";
+                }, 500);
+            }).catch((error) => { 
+                console.error(error);
+            });
+        });
+    }
+});
+
+btnAgregar.addEventListener("click", insertarProducto);
+btnBuscar.addEventListener("click", BuscarEspecialidad);
+btnActualizar.addEventListener("click", actualizarEspecialidad);
+btnMostrar.addEventListener("click", mostrarTablaEspecialidad);
+btnBorrar.addEventListener("click", eliminarEspecialidad);
+
--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getDatabase, onValue, ref as refS, set, child, get, update, remove } from "firebase/database"
+import { getDatabase, onValue, ref as refS, set, child, get, update, remove } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3RXfHp1g81mT8kLq40OfhlEneaDGemgE",
-  authDomain: "proyecto-ede3c.firebaseapp.com",
-  databaseURL: "https://proyecto-ede3c-default-rtdb.firebaseio.com",
-  projectId: "proyecto-ede3c",
-  storageBucket: "proyecto-ede3c.firebasestorage.app",
-  messagingSenderId: "1038275061124",
-  appId: "1:1038275061124:web:dc95692d082f199dfc69d1"
-};
+    apiKey: "AIzaSyB3RXfHp1g81mT8kLq40OfhlEneaDGemgE",
+    authDomain: "proyecto-ede3c.firebaseapp.com",
+    databaseURL: "https://proyecto-ede3c-default-rtdb.firebaseio.com",
+    projectId: "proyecto-ede3c",
+    storageBucket: "proyecto-ede3c.firebasestorage.app",
+    messagingSenderId: "1038275061124",
+    appId: "1:1038275061124:web:dc95692d082f199dfc69d1"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth();
 
 var idPizzas = 0;
 var tamaños = "";
 var uno = "";
 var dos = "";
 var cinco = "";
+
+const btnAnadir = document.getElementById("btnAnadir");
+const btnBuscar = document.getElementById("btnBuscar");
+const btnActualizar = document.getElementById("btnActualizar");
+const btnMostra = document.getElementById("btnMostra");
+const btnBorrar = document.getElementById("btnBorrar");
 
 function limpiarInputs(){
   idPizzas = document.getElementById("txtID").value = "";
@@ -77,3 +85,141 @@ function insertarProducto() {
       alert("Ocurrió un error");
   });
 }
+
+function escribirTamanos(){
+  document.getElementById("txtTamaños").value = tamaños;
+  document.getElementById("txtUNO").value = uno;
+  document.getElementById("txtDOS").value = dos;
+  document.getElementById("txtCINCO").value = cinco;
+}
+
+function BuscarProducto(){
+    let idPizzas = document.getElementById("txtID").value.trim();
+    if(idPizzas === ""){
+        mostrarMensaje("No se ingreso una ID");
+        return;
+    }
+    const dbref = refS(db);
+    get(child(dbref, 'Pizzas/' + idPizzas)).then((snapshot) =>{
+        if(snapshot.exists()){
+            tamaños = snapshot.val().tamaños;
+            uno = snapshot.val().uno;
+            dos = snapshot.val().dos;
+            cinco = snapshot.val().cinco;
+            escribirTamanos();
+        } else {
+            limpiarInputs();
+            mostrarMensaje("El producto con codigo " + idPizzas + "no existe.")
+        }
+    });
+}
+
+function Listarproductos() {
+    const dbref = refS(db, 'Pizzas');
+    const tabla = document.getElementById("tablaTamaños");
+    const tbody = tabla.querySelector("tbody");
+    tbody.innerHTML =
+    `<tr>
+    <th>ID</th>
+    <th>Tamaño</th>
+    <th>1 Ingrediente</th>
+    <th>2-4 Ingredientes</th>
+    <th>5-9 Ingredientes</th>
+    </tr>`;
+
+    onValue(dbref, (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            const childKey = childSnapshot.key;
+            const data = childSnapshot.val();
+
+            var fila = document.createElement("tr");
+
+            // Celda para el número de serie
+            var celdaId = document.createElement("td");
+            celdaId.textContent = data.idPizzas;
+            fila.appendChild(celdaId);
+
+            // Celda para la marca
+            var celdaTamaños = document.createElement("td");
+            celdaTamaños.textContent = data.tamaños;
+            fila.appendChild(celdaTamaños);
+
+            // Celda para el modelo
+            var celdaUno = document.createElement("td");
+            celdaUno.textContent = data.uno;
+            fila.appendChild(celdaUno);
+
+            // Celda para la descripción
+            var celdaDos = document.createElement("td");
+            celdaDos.textContent = data.dos;
+            fila.appendChild(celdaDos);
+
+            // Celda para la imagen
+            var celdaCinco = document.createElement("td");
+            celdaCinco.textContent = data.cinco;
+            fila.appendChild(celdaCinco);
+
+            // Agregar la fila al cuerpo de la tabla
+            tbody.appendChild(fila);
+        });
+    }, { onlyOnce: true });
+}
+
+function actualizarAutomovil(){
+    leerInputs();
+    if(idPizzas ==="" || tamaños ==="" || uno ==="" || dos ==="" || cinco === ""){
+        mostrarMensaje("Favor de capturar toda la informacion.");
+        return;
+    }
+    update(refS(db, 'Pizzas/' + idPizzas),{
+        idPizzas:idPizzas,
+        tamaños:tamaños,
+        uno:uno,
+        dos:dos,
+        cinco:cinco
+    }).then(()=>{
+        mostrarMensaje("Se actualizo con exito.");
+        limpiarInputs();
+        Listarproductos();
+    }).catch((error) =>{
+        mostrarMensaje("Ocurrio un error: " + error)
+    });
+}
+
+function mostrarTabla(){
+    Listarproductos();
+}
+
+function eliminarAutomovil(){
+    let idPizzas = document.getElementById("txtID").value.trim();
+    if(idPizzas === ""){
+        mostrarMensaje("No se ingreso un codigo valido.");
+        return;
+    }
+
+    const dbref = refS(db);
+    get(child(dbref, 'Pizzas/' + idPizzas)).then((snapshot) =>{
+        if(snapshot.exists()){
+            remove(refS(db, 'Pizzas/' + idPizzas))
+            .then(() => {
+                mostrarMensaje("Producto eliminado con exito.");
+                limpiarInputs();
+                Listarproductos();
+            })
+            .catch((error) => {
+                mostrarMensaje("Ocurrio un error al eliminar el producto: " + error);
+            });
+        } else {
+            limpiarInputs();
+            mostrarMensaje("El producto con ID " + idPizzas + "no existe.");
+        }
+    });
+    Listarproductos();
+}
+
+btnAnadir.addEventListener("click", insertarProducto);
+btnBuscar.addEventListener("click", BuscarProducto);
+btnActualizar.addEventListener("click", actualizarAutomovil);
+btnMostra.addEventListener("click", mostrarTabla);
+btnBorrar.addEventListener("click", eliminarAutomovil);
+
